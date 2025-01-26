a98b98ba640895ec4469620b96f7fdf35dda6d29
NikhilEnhancesys <nikhil.kumar@enhancesys.com>
2025-01-26 21:15:12 +0530

    Update FunctionalatiesExecuter.java

--- IPOS/src/test/java/com/leadprogram/FunctionalatiesExecuter.java
+++ IPOS/src/test/java/com/leadprogram/FunctionalatiesExecuter.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 public class FunctionalatiesExecuter extends Launcher {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-
 		Launcher.launch();
 		LeadProgram.leadProgram();
-
 	}
-
 }
